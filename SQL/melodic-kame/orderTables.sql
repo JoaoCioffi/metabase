91f6a7ba46ec9b6c212dac9d771686f60b7c62e8
JoaoCioffi <joaorcioffi@gmail.com>
2022-07-31 14:24:46 -0300

    updated on 2022-07-31 02:24PM

--- SQL/melodic-kame/orderTables.sql
+++ SQL/melodic-kame/orderTables.sql
@@ -1,5 +1,4 @@
-SELECT "source"."ID" AS "ID", "source"."CREATED_AT" AS "CREATED_AT", "source"."age" AS "age", "source"."STATE" AS "STATE", "source"."CITY" AS "CITY", "source"."LATITUDE" AS "LATITUDE", "source"."LONGITUDE" AS "LONGITUDE", "source"."CATEGORY" AS "CATEGORY", "source"."QUANTITY" AS "QUANTITY", "source"."TOTAL" AS "TOTAL"
-FROM (SELECT
+SELECT
     p.id,
     o.created_at,
     (YEAR(CURDATE()) - YEAR(p.birth_date)) AS "age",
@@ -13,4 +12,4 @@ FROM (SELECT
 FROM people p
 LEFT JOIN orders o ON o.user_id = p.id
 LEFT JOIN products prod ON prod.id = o.product_id
-WHERE o.created_at IS NOT NULL) "source"
+WHERE o.created_at IS NOT NULL
