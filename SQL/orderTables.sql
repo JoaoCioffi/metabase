16a5c937b0ba36fca24aba407fe51b51bc414e4e
João Cioffi <60454486+JoaoCioffi@users.noreply.github.com>
2022-07-31 14:09:01 -0300

    Update orderTables.sql

--- SQL/orderTables.sql
+++ SQL/orderTables.sql
@@ -1,1 +1,17 @@
-
+SELECT "source"."ID" AS "ID", "source"."CREATED_AT" AS "CREATED_AT", "source"."age" AS "age", "source"."STATE" AS "STATE", "source"."CITY" AS "CITY", "source"."LATITUDE" AS "LATITUDE", "source"."LONGITUDE" AS "LONGITUDE", "source"."CATEGORY" AS "CATEGORY", "source"."QUANTITY" AS "QUANTITY", "source"."TOTAL" AS "TOTAL"
+FROM (SELECT
+    p.id,
+    o.created_at,
+    (YEAR(CURDATE()) - YEAR(p.birth_date)) AS "age",
+    p.state,
+    p.city,
+    p.latitude,
+    p.longitude,
+    prod.category,
+    o.quantity,
+    o.total
+FROM people p
+LEFT JOIN orders o ON o.user_id = p.id
+LEFT JOIN products prod ON prod.id = o.product_id
+WHERE o.created_at IS NOT NULL) "source"
+LIMIT 1048575
